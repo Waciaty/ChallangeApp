58e906391ffa3ebbbcc69e7934109b0a0246b390
Kamil Preuss <preusskamil13@gmail.com>
2023-06-11 12:41:53 +0100

    Zadanie 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.Design;
+
+var name = "Ewa";
+var isMale = false;
+var age = 33;
+
+
+if (age < 30 && isMale == false)
+{
+    Console.WriteLine("Kobieta ponizej lat 30");
+}
+else if (age == 33 && name == "Ewa" && isMale == false)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && isMale == true)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else if (age == 40 && isMale == true && name == "Kamil")
+{
+    Console.WriteLine("Kamil, lat 40");
+}
+
+
